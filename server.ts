7bef8f0af5283daa00fa09f790998b86a6540322
Sean aye <hello@seanaye.ca>
2021-02-08 13:19:21 -0500

    update server setup

--- server.ts
+++ server.ts
@@ -1,61 +1,88 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
-const { createServer: createViteServer } = require('vite')
 
-async function createServer() {
+const isTest = process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD
+
+async function createServer(
+  root = process.cwd(),
+  isProd = process.env.NODE_ENV === 'production'
+) {
+  const resolve = (p) => path.resolve(__dirname, p)
+
+  const indexProd = isProd
+    ? fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
+    : ''
+
+  const manifest = isProd
+    ? // @ts-ignore
+      require('./dist/client/ssr-manifest.json')
+    : {}
+
   const app = express()
 
-  // Create vite server in middleware mode. This disables Vite's own HTML
-  // serving logic and let the parent server take control.
-  const vite = await createViteServer({
-    server: { middlewareMode: true }
-  })
-  // use vite's connect instance as middleware
-  app.use(vite.middlewares)
+  /**
+   * @type {import('vite').ViteDevServer}
+   */
+  let vite
+  if (!isProd) {
+    vite = await require('vite').createServer({
+      root,
+      logLevel: isTest ? 'error' : 'info',
+      server: {
+        middlewareMode: true
+      }
+    })
+    // use vite's connect instance as middleware
+    app.use(vite.middlewares)
+  } else {
+    app.use(require('compression')())
+    app.use(
+      require('serve-static')(resolve('dist/client'), {
+        index: false
+      })
+    )
+  }
 
   app.use('*', async (req, res) => {
-    // serve index.html - we will tackle this next
-    const url = req.originalUrl
-
     try {
-      // 1. Read index.html
-      let template = fs.readFileSync(
-        path.resolve(__dirname, 'index.html'),
-        'utf-8'
-      )
+      const url = req.originalUrl
 
-      // 2. Apply vite HTML transforms. This injects the vite HMR client, and
-      //    also applies HTML transforms from Vite plugins, e.g. global preambles
-      //    from @vitejs/plugin-react-refresh
-      template = await vite.transformIndexHtml(url, template)
+      let template, render
+      if (!isProd) {
+        // always read fresh template in dev
+        template = fs.readFileSync(resolve('index.html'), 'utf-8')
+        template = await vite.transformIndexHtml(url, template)
+        render = (await vite.ssrLoadModule('/src/entry-server.ts')).render
+      } else {
+        template = indexProd
+        render = require('./dist/server/entry-server.ts').render
+      }
 
-      // 3. Load the server entry. vite.ssrLoadModule automatically transforms
-      //    your ESM source code to be usable in Node.js! There is no bundling
-      //    required, and provides efficient invalidation similar to HMR.
-      const { render } = await vite.ssrLoadModule('/src/entry-server.ts')
+      const [appHtml, preloadLinks] = await render(url, manifest)
 
-      // 4. render the app HTML. This assumes entry-server.js's exported `render`
-      //    function calls appropriate framework SSR APIs,
-      //    e.g. ReacDOMServer.renderToString()
-      const appHtml = await render(url)
+      const html = template
+        .replace(`<!--preload-links-->`, preloadLinks)
+        .replace(`<!--app-html-->`, appHtml)
 
-      // 5. Inject the app-rendered HTML into the template.
-      const html = template.replace(`<!--ssr-outlet-->`, appHtml)
-
-      // 6. Send the rendered HTML back.
       res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
     } catch (e) {
-      // If an error is caught, let vite fix the stracktrace so it maps back to
-      // your actual source code.
-      vite.ssrFixStacktrace(e)
-      console.error(e)
-      res.status(500).end(e.message)
+      vite && vite.ssrFixStacktrace(e)
+      console.log(e.stack)
+      res.status(500).end(e.stack)
     }
   })
 
-  app.listen(3000)
+  return { app, vite }
 }
 
-createServer()
+if (!isTest) {
+  createServer().then(({ app }) =>
+    app.listen(3000, () => {
+      console.log('http://localhost:3000')
+    })
+  )
+}
 
+// for test use
+exports.createServer = createServer
