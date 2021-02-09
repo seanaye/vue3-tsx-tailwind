dc6b098cf3cdd78cfcaf571e0f4937c25ae2390e
Sean aye <hello@seanaye.ca>
2021-02-09 11:37:19 -0500

    update page discovery

--- src/router.js
+++ src/router.js
@@ -3,26 +3,19 @@ import {
   createRouter as _createRouter,
   createWebHistory
 } from 'vue-router'
-import { Home } from './pages/Home'
 
 // Auto generates routes from vue files under ./pages
 // https://vitejs.dev/guide/features.html#glob-import
-//const pages = import.meta.glob('./pages/*.tsx')
+const pages = import.meta.glob('./pages/*.tsx')
+console.log({ pages })
 
-//const routes = Object.keys(pages).map((path) => {
-//  const name = path.match(/\.\/pages(.*)\.tsx$/)[1].toLowerCase()
-//  console.log({ name })
-//  return {
-//    path: name === '/home' ? '/' : name,
-//    component: pages[path] // () => import('./pages/*.vue')
-//  }
-//})
-//
-
-
-const routes = [
-  { component: Home, path: '/' }
-]
+const routes = Object.keys(pages).map((path) => {
+  const name = path.match(/\.\/pages(.*)\.tsx$/)[1].toLowerCase()
+  return {
+    path: name === '/home' ? '/' : name,
+    component: pages[path] // () => import('./pages/*.vue')
+  }
+})
 
 export function createRouter() {
   return _createRouter({
